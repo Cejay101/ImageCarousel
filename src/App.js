57472e4d1975db4140a245c8cb85574ee4cb710f
cejay101 <clintonjoyfimie@gmail.com>
2023-04-21 12:38:10 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import Carousel from "./Carousel";
+const images = [
+  "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
+  "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+];
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Image Carousel using React and Framer Motion</h1>
+      </header>
+      <main>
+        <Carousel images={images} />
+      </main>
+    </div>
+  );
+}
+export default App;
